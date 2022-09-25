c9f38926583cac0a73e406acdd05ed3c008e357e
Marian Zburlea <marian@wowdop.com>
2022-09-25 21:52:43 +0100

    nici nu stiu cum se scrie: FETCH

--- src/component/my-cv/my-cv.jsx
+++ src/component/my-cv/my-cv.jsx
@@ -1,6 +1,21 @@
-import { Button, Header, Reset } from "..";
+import { Fragment, useEffect, useState } from 'react'
+import { Button, Header, Reset } from '..'
+import { get } from 'axios'
 
 const MyCV = () => {
+  const [colectia, modificaColectia] = useState([])
+
+  useEffect(() => {
+    const ggg = async () => {
+      const { data } = await get(
+        'https://api.github.com/users/marianzburlea/repos'
+      )
+      modificaColectia(data)
+    }
+
+    ggg()
+  }, [])
+
   return (
     <div>
       <Reset />
@@ -9,6 +24,16 @@ const MyCV = () => {
         <span>O</span>
         Download
       </Button>
+      <ul>
+        {colectia.map(({ language, description, id }) => (
+          <Fragment key={id}>
+            <li>{description}</li>
+            <li>{language}</li>
+            <li>{id}</li>
+            <hr />
+          </Fragment>
+        ))}
+      </ul>
       <Header>Tech I love to work with</Header>
       <Button color="milanoRed">Subscribe</Button>
       <Header font={16} color="funOrange">
@@ -24,7 +49,7 @@ const MyCV = () => {
       <Button color="funGreen">Bonus</Button>
       <p>Neata, ne trezim?</p>
     </div>
-  );
-};
+  )
+}
 
-export default MyCV;
+export default MyCV
